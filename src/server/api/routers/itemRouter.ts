adcfab6d942961dabad23ab748354ffe2a5b3b0a
Xila Luna <xilaluna2@gmail.com>
2023-01-24 23:17:07 -0800

    [add] item router

---
+++ src/server/api/routers/itemRouter.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const itemRouter = createTRPCRouter({
+  addItem: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.item.create({
+        data: { name: input.name, checked: false },
+      });
+    }),
+});
