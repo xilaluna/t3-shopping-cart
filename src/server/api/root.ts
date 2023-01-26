9efa80da3441987c72d188e22726577646707e4c
Xila Luna <xilaluna2@gmail.com>
2023-01-26 10:34:41 -0800

    update routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { itemRouter } from "./routers/itemRouter";
 
 /**
@@ -8,7 +7,6 @@ import { itemRouter } from "./routers/itemRouter";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   itemRouter: itemRouter,
 });
 
